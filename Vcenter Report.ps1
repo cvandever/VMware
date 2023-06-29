e8c206241cd9193808c70d3c01c133b31c4c40dd
Cody Vandever <cvandever@ceriumnetworks.com>
2023-06-29 11:39:03 -0700

    Update to Excel with multi tab, Changes Snapshot details update filenaming.

--- Vcenter Report.ps1
+++ Vcenter Report.ps1
@@ -1,3 +1,9 @@
+# Install the ImportExcel module if not already installed
+if (-not (Get-Module -Name ImportExcel -ListAvailable)) {
+    Install-Module -Name ImportExcel -Scope CurrentUser -Force
+}
+
+
 function Get-ServerName {
     param (
         [string]$DefaultServerName = 'spolabvcsa.lab.ceriumnetworks.com'
@@ -14,25 +20,21 @@ function Get-ServerName {
 
 function Get-SaveFileDialog {
     param (
-        [string]$ServerName,
-        [string]$DefaultFolder = 'C:\Reports'
+        [string]$ServerName
     )
 
     Add-Type -AssemblyName System.Windows.Forms
-
     # Get the current date and time
     $currentDateTime = Get-Date -Format 'MMddyy_HHmm'
-
     # Get the current working directory
     $defaultFolder = (Get-Item -Path ".\").FullName
-
     # Create the default filename using the server name and current date/time
-    $defaultFileName = "$ServerName" + "_$currentDateTime.csv"
+    $defaultFileName = "$ServerName" + "_$currentDateTime.xlsx"
 
     # Create an instance of the SaveFileDialog
     $fileDialog = New-Object System.Windows.Forms.SaveFileDialog
-    $fileDialog.Filter = 'CSV Files (*.csv)|*.csv'
-    $fileDialog.Title = 'Save CSV File'
+    $fileDialog.Filter = 'Excel Files (*.xlsx)|*.xlsx'
+    $fileDialog.Title = 'Save Excel File'
     $fileDialog.InitialDirectory = $DefaultFolder
     $fileDialog.FileName = $defaultFileName
 
@@ -40,13 +42,11 @@ function Get-SaveFileDialog {
     $dialogResult = $fileDialog.ShowDialog()
 
     # Check if the user clicked the OK button
-    
     if ($dialogResult -eq 'OK') {
         return $fileDialog.FileName
     }
     return $null
 }
-
 
 function Get-VMNetworkInfo {
     param (
@@ -56,13 +56,14 @@ function Get-VMNetworkInfo {
 
     process {
         $networkAdapter = $VM | Get-NetworkAdapter
+        $connected = $networkAdapter.ConnectionState.Connected -join ', '
         $portgroup = $networkAdapter.NetworkName -join ', '
         $macAddress = $networkAdapter.MacAddress -join ', '
         $adapterType = $networkAdapter.Type -join ', '
 
         [PSCustomObject]@{
             PortGroup = $portgroup
-            NetworkAdapter = $networkAdapter.NetworkName
+            ConnectionState = $connected
             AdapterType = $adapterType
             MacAddress = $macAddress
         }
@@ -81,20 +82,34 @@ function Get-VMSnapshotInfo {
         if ($snapshots) {
             $snapshotNames = $snapshots.Name -join ', '
             $snapshotCreated = $snapshots.Created.Date -join ', '
-            $snapshotSize = "{0:N2}" -f ($snapshots.SizeGB -join ', ')
+            if ($snapshots.SizeGB -gt 0.01) {
+                $CumSnapshotSize = "{0:N2}GB" -f ($snapshots | Measure-Object -Property SizeGB -Sum).Sum
+            }
+            else {
+                $CumSnapshotSize = "~0.01GB"
+            }
 
             [PSCustomObject]@{
                 SnapshotName = $snapshotNames
                 SnapshotCreated = $snapshotCreated
-                SnapshotSize = $snapshotSize
-            }
-        } else {
-            [PSCustomObject]@{
-                SnapshotName = $null
-                SnapshotCreated = $null
-                SnapshotSize = $null
+                CumulativeSnapshotSize = $CumSnapshotSize
             }
         }
+    }
+}
+
+function Get-VMEventUsers {
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        $VM
+    )
+
+    process {
+        $events = Get-VIEvent -Entity $VM -MaxSamples ([int]::MaxValue)
+        $users = $events | Where-Object { $_.UserName -like "CERIUMNETWORKS\*" } | ForEach-Object {
+            $_.UserName -replace "^CERIUMNETWORKS\\"
+        } | Select-Object -Unique
+        return $users -join ', '
     }
 }
 
@@ -108,19 +123,28 @@ Connect-VIServer -Server $ServerName  -Credential $creds
 
 $allVMs = Get-VM
 
+# Create an array to store the worksheets
+$worksheets = @()
+
 # If you add more columns, make sure to add them to the $columns variable below
 # Creates ordered hashtable with column names and values. In order left to right
-$columns = ('Name', 'PowerState', 'GuestOS', 'HostName','ResourcePool',
-    'Datastore', 'vCPUs', 'MemorySize', 'ProvisionedStorage', 'PercentUsed',
-    'IPAddress', 'PortGroup', 'NetworkAdapter', 'AdapterType', 'MacAddress',
-    'SnapshotName', 'SnapshotCreated', 'SnapshotSize', 'VMToolsVersion',
-    'VMOwner', 'VMOwnerTeam', 'VMCreation', 'VMExpiration')
+$columnsGeneralInfo = ('Name', 'PowerState', 'GuestOS', 'HostName','ResourcePool',
+    'Datastore', 'vCPUs', 'MemorySize', 'ProvisionedStorage', 'PercentStorageUsed',
+    'VMToolsVersion', 'VMOwner', 'VMOwnerTeam', 'VMCreation', 'VMExpiration', 'RecentUsers' )
 
-    $exportData = foreach ($vm in $allVMs) {
-        $networkInfo = $vm | Get-VMNetworkInfo
-        $snapshotInfo = $vm | Get-VMSnapshotInfo
+
+$columnsNetworkInfo = ('Name', 'IPAddress', 'PortGroup', 'ConnectionState', 'AdapterType', 'MacAddress')
+
+
+$columnsSnapshotInfo = ('Name', 'SnapshotName', 'SnapshotCreated', 'CumulativeSnapshotSize')
+
+
+foreach ($vm in $allVMs) {
+    $networkInfo = $vm | Get-VMNetworkInfo
+    $snapshotInfo = $vm | Get-VMSnapshotInfo
+    #$users = Get-VMEventUsers -VM $vm
     
-    [PSCustomObject]@{
+    $generalInfoData = [PSCustomObject]@{
         Name = $vm.Name
         PowerState = $vm.PowerState
         GuestOS = $vm.Guest.OSFullName
@@ -129,21 +153,47 @@ $columns = ('Name', 'PowerState', 'GuestOS', 'HostName','ResourcePool',
         Datastore = ($vm | Get-Datastore).Name -join ', '
         vCPUs = $vm.NumCpu
         MemorySize = $vm.MemoryGB
-        ProvisionedStorage = "{0:N2}" -f $vm.ProvisionedSpaceGB
-        PercentUsed = "{0:N2}" -f ($vm.UsedSpaceGB / $vm.ProvisionedSpaceGB * 100)
-        IPAddress = $vm.Guest.IPAddress -join ', '
-        PortGroup = $networkInfo.PortGroup
-        NetworkAdapter = $networkInfo.NetworkAdapter
-        AdapterType = $networkInfo.AdapterType
-        MacAddress = $networkInfo.MacAddress
-        SnapshotName = $snapshotInfo.SnapshotName
-        SnapshotCreated = $snapshotInfo.SnapshotCreated
-        SnapshotSize = $snapshotInfo.SnapshotSize
+        ProvisionedStorage = "{0:N2}GB" -f $vm.ProvisionedSpaceGB
+        PercentStorageUsed = "{0:N2}%" -f ($vm.UsedSpaceGB / $vm.ProvisionedSpaceGB * 100)
         VMToolsVersion = $vm.Guest.ToolsVersion
         VMOwner = $vm.CustomFields["VM Owner"]
         VMOwnerTeam = $vm.CustomFields["Owner's Team"]
         VMCreation = $vm.CustomFields["Creation Date"]
         VMExpiration = $vm.CustomFields["Expiration Date"]
+        #RecentUsers = $users
+    }
+    $networkInfoData = [PSCustomObject]@{
+        Name = $vm.Name
+        IPAddress = $vm.Guest.IPAddress -join ', '
+        PortGroup = $networkInfo.PortGroup
+        ConnectionState = $networkInfo.ConnectionState
+        AdapterType = $networkInfo.AdapterType
+        MacAddress = $networkInfo.MacAddress
+    }
+        
+    $snapshotInfoData = [PSCustomObject]@{
+        Name = $vm.Name
+        SnapshotName = $snapshotInfo.SnapshotName
+        SnapshotCreated = $snapshotInfo.SnapshotCreated
+        CumulativeSnapshotSize = $snapshotInfo.CumulativeSnapshotSize    
+    }
+
+    $worksheets += @{
+        Name = 'General System Info'
+        Data = $generalInfoData
+        Columns = $columnsGeneralInfo
+    }
+
+    $worksheets += @{
+        Name = 'Network Info'
+        Data = $networkInfoData
+        Columns = $columnsNetworkInfo
+    }
+
+    $worksheets += @{
+        Name = 'Snapshot Info'
+        Data = $snapshotInfoData
+        Columns = $columnsSnapshotInfo
     }
 }
     
@@ -151,6 +201,13 @@ Disconnect-VIServer -Server $ServerName -Confirm:$false
 Write-Host "Disconnected from $ServerName"
 Write-Host "Exporting data to $Path"
 
-$selectedProperties = $exportData | Select-Object -Property $columns
-$selectedProperties | Export-Csv -Path $Path -NoTypeInformation -Append
+ForEach ($worksheet in $worksheets) {
+    $worksheetName = $worksheet.Name
+    $worksheetData = $worksheet.Data
+    $worksheetColumns = $worksheet.Columns
+
+    $worksheetData | Select-Object -Property $worksheetColumns | Export-Excel -Path $Path -WorksheetName $worksheetName -AutoSize -TableStyle 'Medium1' `
+        -BoldTopRow -FreezeTopRow -AutoFilter -AutoNameRange -TableName "Table $worksheetName" `
+        -Append 
+}
 
